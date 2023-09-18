a7df396de2ce1abbaf3cc0e39fd21672adc99539
malt9830 <malt9830@stud.kea.dk>
2023-09-18 12:00:15 +0200

    Fixed highlight occuring on all but chosen part

--- src/components/HouseSelector/CrestPart.jsx
+++ src/components/HouseSelector/CrestPart.jsx
@@ -1,9 +1,10 @@
-import { animateHouse } from "../../utils/housing"
+import { animateHouse, unanimateHouse } from "../../utils/housing"
 
 export default function CrestPart({ house }) {
   return (
     <img
       onMouseEnter={() => animateHouse(house)}
+      onMouseLeave={() => unanimateHouse(house)}
       id={`${house}-part`}
       className={`hogwarts-crest-${house}`}
       src={`src/assets/hogwarts-parts/hogwarts-mascots/hogwarts-${house}.svg`}
--- src/utils/housing.js
+++ src/utils/housing.js
@@ -1,21 +1,21 @@
 const houses = ["gryffindor", "slytherin", "hufflepuff", "ravenclaw"];
 
-export function animateHouse(house) {
+export function animateHouse(part) {
   // Add highlight
   houses.forEach((house) => {
-    if (exception !== house || exception === "hogwarts") document.querySelector(`#${house}-colour`).classList.add("opacity-50");
+    if (part !== house || part === "hogwarts") document.querySelector(`#${house}-colour`).classList.add("opacity-50");
   });
 
   // Animate part
-  document.querySelector(`#${house}-part`).classList.add("animate");
+  document.querySelector(`#${part}-part`).classList.add("animate");
 }
 
-export function unanimateHouse(house) {
+export function unanimateHouse(part) {
   // Remove hightlight
   houses.forEach((house) => {
     document.querySelector(`#${house}-colour`).classList.remove("opacity-50");
   });
 
   // Unanimate part
-  document.querySelector(`#${house}-part`).classList.remove("animate");
+  document.querySelector(`#${part}-part`).classList.remove("animate");
 }
