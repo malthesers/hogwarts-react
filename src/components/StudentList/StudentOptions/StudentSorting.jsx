306258cd860525767b490352c14a08635d28cb78
malt9830 <malt9830@stud.kea.dk>
2023-09-19 17:47:00 +0200

    Added toggling for showing sorting methods

--- src/components/StudentList/StudentOptions/StudentSorting.jsx
+++ src/components/StudentList/StudentOptions/StudentSorting.jsx
@@ -16,11 +16,11 @@ export default function StudentFilters() {
   return (
     <div className="grid gap-2 sm:gap-4 grid-cols-[auto_4rem] sm:grid-cols-[7rem_1fr]">
       <div className="cursor-pointer">
-        <p className={`bg-${theme}-dark border-${theme}-accent` + " sm:hidden border-2 p-2 flex justify-between items-center"}>
+        <p onClick={() => setShowSortingMethods(!showSortingMethods)} className={`bg-${theme}-dark border-${theme}-accent` + " sm:hidden border-2 p-2 flex justify-between items-center"}>
           <span>Sort by...</span>
           <IconChevron className={`fill-${theme}-accent` + " h-4 duration-300"} />
         </p>
-          <div className="sm:hidden max-h-40 overflow-hidden">
+          <div className={(showSortingMethods ? 'max-h-40' : 'max-h-0') + " sm:hidden overflow-hidden duration-[250ms]"}>
             { Object.keys(sortingMethods).map((key) =>
               <p key={key} className={`bg-${theme}-dark border-${theme}-accent` + " border-2 border-t-0 p-2 flex justify-between items-center"}>
                 <span>{ sortingMethods[key] }</span>
