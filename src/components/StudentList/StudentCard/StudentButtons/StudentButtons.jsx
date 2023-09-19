4d272cb8becb4fdec130b39487fff5e55222e822
malt9830 <malt9830@stud.kea.dk>
2023-09-19 11:42:00 +0200

    Added fading when buttons are disabled

--- src/components/StudentList/StudentCard/StudentButtons/StudentButtons.jsx
+++ src/components/StudentList/StudentCard/StudentButtons/StudentButtons.jsx
@@ -4,7 +4,7 @@ import ExpelButton from "./ExpelButton"
 
 export function StudentButtons({ student }) {
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2 my-4 duration-200">
+    <div className={(student.expelled ? 'opacity-30' : '') + ' grid sm:grid-cols-2 md:grid-cols-3 gap-2 my-4 duration-200'}>
       <PrefectButton student={student} />
       <InquisitorButton student={student} />
       <ExpelButton student={student} />
