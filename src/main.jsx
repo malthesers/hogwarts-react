efae89651b4b2f825563f6fbf965e957af00a9ad
malt9830 <malt9830@stud.kea.dk>
2023-09-21 11:57:30 +0200

    Added MessagesProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from './context/ThemeContext.jsx'
 import { StudentsProvider } from './context/StudentsContext.jsx'
+import { MessagesProvider } from './context/MessagesContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <StudentsProvider>
-        <App />
+        <MessagesProvider>
+          <App />
+        </MessagesProvider>
       </StudentsProvider>
     </ThemeProvider>
   </React.StrictMode>,
