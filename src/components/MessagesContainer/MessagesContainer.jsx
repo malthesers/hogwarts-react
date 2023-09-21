d6c353194e3a054b0fa417599d02f96c0034cfb1
malt9830 <malt9830@stud.kea.dk>
2023-09-21 11:57:51 +0200

    Updated class to className in MessagesContainer

--- src/components/MessagesContainer/MessagesContainer.jsx
+++ src/components/MessagesContainer/MessagesContainer.jsx
@@ -4,14 +4,14 @@ export default function MessagesContainer() {
   const theme = useTheme()
 
   return (
-    <div class="fixed z-10 top-0 left-0 w-full h-full p-4 pointer-events-none">
-      <div class="relative max-w-2xl mx-auto">
-        {/* <div v-for="message in messages" :key="message" :class="`bg-${theme}-dark border-${theme}-accent`" class="absolute w-full p-2 flex justify-between border-2 rounded-lg">
+    <div className="fixed z-10 top-0 left-0 w-full h-full p-4 pointer-events-none">
+      <div className="relative max-w-2xl mx-auto">
+        {/* <div v-for="message in messages" :key="message" :className="`bg-${theme}-dark border-${theme}-accent`" className="absolute w-full p-2 flex justify-between border-2 rounded-lg">
           <div>
-            <p class="text-md sm:text-2xl">{{ message.title }}</p>
-            <p class="text-sm sm:text-lg">{{ message.description }}</p>
+            <p className="text-md sm:text-2xl">{{ message.title }}</p>
+            <p className="text-sm sm:text-lg">{{ message.description }}</p>
           </div>
-          <img src="../assets/icons/messenger-owl.svg" alt="messenger owl" class="w-12 place-self-center">
+          <img src="../assets/icons/messenger-owl.svg" alt="messenger owl" className="w-12 place-self-center">
         </div> */}
       </div>
     </div>
