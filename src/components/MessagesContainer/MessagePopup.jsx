11b9859fe9e1266ba405c19abf21c079c0495905
malt9830 <malt9830@stud.kea.dk>
2023-09-21 12:05:59 +0200

    Added MessagePopup component

---
+++ src/components/MessagesContainer/MessagePopup.jsx
@@ -0,0 +1,15 @@
+import { useTheme } from "../../context/ThemeContext"
+
+export default function MessagePopup({ message }) {
+  const { theme } = useTheme()
+
+  return (
+    <div className={`bg-${theme}-dark border-${theme}-accent` + ' absolute w-full p-2 flex justify-between border-2 rounded-lg'}>
+      <div>
+        <p className="text-md sm:text-2xl">{ message.title }</p>
+        <p className="text-sm sm:text-lg">{ message.description }</p>
+      </div>
+      <img src="../assets/icons/messenger-owl.svg" alt="messenger owl" className="w-12 place-self-center"/>
+    </div>
+  )
+}
--- src/components/MessagesContainer/MessagesContainer.jsx
+++ src/components/MessagesContainer/MessagesContainer.jsx
@@ -1,18 +1,15 @@
-import { useTheme } from "../../context/ThemeContext"
+import { useMessages } from "../../context/MessagesContext"
+import MessagePopup from "./MessagePopup"
 
 export default function MessagesContainer() {
-  const theme = useTheme()
+  const { messages } = useMessages()
 
   return (
     <div className="fixed z-10 top-0 left-0 w-full h-full p-4 pointer-events-none">
       <div className="relative max-w-2xl mx-auto">
-        {/* <div v-for="message in messages" :key="message" :className="`bg-${theme}-dark border-${theme}-accent`" className="absolute w-full p-2 flex justify-between border-2 rounded-lg">
-          <div>
-            <p className="text-md sm:text-2xl">{{ message.title }}</p>
-            <p className="text-sm sm:text-lg">{{ message.description }}</p>
-          </div>
-          <img src="../assets/icons/messenger-owl.svg" alt="messenger owl" className="w-12 place-self-center">
-        </div> */}
+        { messages.map((message) =>
+          <MessagePopup key={message} message={message} />
+        )}
       </div>
     </div>
   )
