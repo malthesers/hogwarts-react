5484a02c3b6d0932693e37c68c20248803bcdf37
malt9830 <malt9830@stud.kea.dk>
2023-09-22 15:38:49 +0200

    Added HackingProvider wrapper to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { StudentsProvider } from './context/StudentsContext.jsx'
 import { MessagesProvider } from './context/MessagesContext.jsx'
+import { HackingProvider } from './context/HackingContext.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <StudentsProvider>
-        <MessagesProvider>
-          <App />
-        </MessagesProvider>
+        <HackingProvider>
+          <MessagesProvider>
+            <App />
+          </MessagesProvider>
+        </HackingProvider>
       </StudentsProvider>
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
