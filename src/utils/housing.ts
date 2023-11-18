6458305e71a5178d549b07a5a2fc6b132f78594e
malt9830 <malt9830@stud.kea.dk>
2023-11-19 00:39:26 +0100

    Implemented ts in housing

---
+++ src/utils/housing.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+import { House } from "../interfaces/House";
+
+const houses:House[] = ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw'];
+
+export function animateHouse(part: House) {
+  // Add highlight
+  houses.forEach((house) => {
+    if (part !== house || part === 'hogwarts') document.querySelector(`#${house}-colour`)?.classList.add('opacity-50');
+  });
+
+  // Animate part
+  document.querySelector(`#${part}-part`)?.classList.add('animate');
+}
+
+export function unanimateHouse(part: House) {
+  // Remove hightlight
+  houses.forEach((house) => {
+    document.querySelector(`#${house}-colour`)?.classList.remove('opacity-50');
+  });
+
+  // Unanimate part
+  document.querySelector(`#${part}-part`)?.classList.remove('animate');
+}
