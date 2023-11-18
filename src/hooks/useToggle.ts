ae24b4e327bd2950690798fd238b9f0355e00025
malt9830 <malt9830@stud.kea.dk>
2023-11-18 04:46:26 +0100

    Implemented ts in useToggle

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function useToggle(defaultValue: boolean) {
+  const [value, setValue] = useState<boolean>(defaultValue);
+
+  function toggleValue(value: boolean): void {
+    setValue((currentValue) => (typeof value === 'boolean' ? value : !currentValue));
+  }
+
+  return [value, toggleValue];
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
-import { StudentsProvider } from './context/StudentsContext.jsx'
-import { MessagesProvider } from './context/MessagesContext.jsx'
-import { HackingProvider } from './context/HackingContext.jsx'
-import { ThemeProvider } from './context/ThemeContext.jsx'
+import { StudentsProvider } from './context/StudentsContext'
+import { MessagesProvider } from './context/MessagesContext'
+import { HackingProvider } from './context/HackingContext'
+import { ThemeProvider } from './context/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
