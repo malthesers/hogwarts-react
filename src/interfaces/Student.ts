7a63b1474018223b4ac42a0c220183b23a9debac
malt9830 <malt9830@stud.kea.dk>
2023-11-18 05:18:49 +0100

    Added Student interface

---
+++ src/interfaces/Student.ts
@@ -0,0 +1,19 @@
+import { House } from "./House";
+
+export interface Student {
+    fullName: string,
+    firstName: string,
+    middleName: string | undefined,
+    nickName: string | undefined,
+    lastName: string,
+    gender: 'Male' | 'Female',
+    house: House,
+    photo: `${string}.png`,
+    bloodStatus: BloodStatus,
+    captain: boolean,
+    prefect: boolean,
+    expelled: boolean,
+    inquisitor: boolean,
+}
+
+export type BloodStatus = 'Pure-blood' | 'Muggle-born' | 'Half-blood' | 'Squib'
