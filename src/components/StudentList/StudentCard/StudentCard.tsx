ccb99b4c598132ce4a637199626dfd9e0c43c0c1
malt9830 <malt9830@stud.kea.dk>
2023-11-19 21:39:17 +0100

    Implemented ts in StudentCard

--- src/components/StudentList/StudentCard/StudentCard.tsx
+++ src/components/StudentList/StudentCard/StudentCard.tsx
@@ -1,12 +1,12 @@
+import { Student } from '../../../interfaces/Student';
 import useToggle from '../../../hooks/useToggle'
 import StudentDetails from './StudentDetails'
-import PropTypes from 'prop-types';
 
-StudentCard.propTypes = {
-  student: PropTypes.object
+interface StudentCardProps {
+  student: Student
 }
 
-export default function StudentCard({ student }) {
+export default function StudentCard({ student }: StudentCardProps) {
   const [showDetails, toggleDetails] = useToggle(false)
 
   return (
@@ -14,7 +14,7 @@ export default function StudentCard({ student }) {
       <div className='absolute top-0 left-0 p-4 grid w-full h-full'>
         <img src={`./images/crests/${student.house.toLowerCase()}.svg`} className='w-2/3 lg:w-1/4 m-auto opacity-20'/>
       </div>
-      <div onClick={toggleDetails} className='relative grid grid-cols-[7rem_1fr] gap-4 cursor-pointer'>
+      <div onClick={() => toggleDetails()} className='relative grid grid-cols-[7rem_1fr] gap-4 cursor-pointer'>
         <img src={`./images/students/${student.photo}`} alt={`portrait of ${student.firstName} ${student.lastName}`} className='rounded-[26px]'/>
         <div className='text-xl flex flex-col items-start justify-between my-2 md:text-2xl sm:grid sm:grid-cols-3 sm:items-center'>
           <p className='px-2'>{ student.firstName }</p>
