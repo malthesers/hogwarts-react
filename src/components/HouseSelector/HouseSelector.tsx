b07aa1879c53e9c515409154542b7eb3b1721723
malt9830 <malt9830@stud.kea.dk>
2023-11-19 22:01:53 +0100

    Implemented ts in HouseSelector

--- src/components/HouseSelector/HouseSelector.tsx
+++ src/components/HouseSelector/HouseSelector.tsx
@@ -1,25 +1,25 @@
 import { useEffect, useRef } from 'react';
 import { animateHouse, unanimateHouse } from '../../utils/housing';
-import { useHacking } from '../../context/';
-import { useTheme } from '../../context/';
+import { House } from '../../interfaces/House';
+import { useHacking } from '../../context';
+import { useTheme } from '../../context';
 import useToggle from '../../hooks/useToggle';
 import CrestColour from './CrestColour';
 import CrestPart from './CrestPart';
-import PropTypes from 'prop-types';
 
-HouseSelector.propTypes = {
-  showHouseSelector: PropTypes.bool,
-  toggleHouseSelector: PropTypes.func
+interface HouseSelectorProps {
+  showHouseSelector: boolean,
+  toggleHouseSelector: (value?: boolean) => void
 }
 
-export default function HouseSelector({ showHouseSelector, toggleHouseSelector }) {
+export default function HouseSelector({ showHouseSelector, toggleHouseSelector }: HouseSelectorProps) {
   const [animating, toggleAnimating] = useToggle(true)
   const { isHacked } = useHacking()
   const { theme } = useTheme()
-  const mist = useRef(null)
+  const mist = useRef<HTMLImageElement>(null)
   
   function animateCrest() {
-    const housesOrder = ['gryffindor', 'slytherin', 'ravenclaw', 'hufflepuff', 'hogwarts', '']
+    const housesOrder:(House | '')[] = ['gryffindor', 'slytherin', 'ravenclaw', 'hufflepuff', 'hogwarts', '']
     toggleAnimating(true)
 
     housesOrder.forEach((house, index) => {
@@ -37,7 +37,7 @@ export default function HouseSelector({ showHouseSelector, toggleHouseSelector }
   useEffect(() => {
     if (isHacked) {
       toggleAnimating(true)
-      mist.current.classList.add('appear')
+      mist.current?.classList.add('appear')
     }
   }, [isHacked])
 
@@ -48,7 +48,7 @@ export default function HouseSelector({ showHouseSelector, toggleHouseSelector }
   return (
     <footer className={ (showHouseSelector ? 'h-full' : 'h-20') + ` bg-${theme}-dark` + ' fixed z-20 bottom-0 w-full grid place-content-center duration-500' }>
       <p className={ (showHouseSelector ? 'scale-100 mb-8' : 'scale-0 mb-0') + ' text-3xl sm:text-4xl text-center duration-200'}>Select a House</p>
-      <div onClick={toggleHouseSelector} className={ (animating ? '[&>*]:pointer-events-none pointer-events-none ' : '[&>*]:cursor-pointer  cursor-pointer ') + (showHouseSelector ? 'w-[20rem] sm:w-[30rem]' : '[&>*]:pointer-events-none w-[6rem] mb-20') + ' hogwarts-crest-container grid mx-auto duration-500'}>
+      <div onClick={() => toggleHouseSelector()} className={ (animating ? '[&>*]:pointer-events-none pointer-events-none ' : '[&>*]:cursor-pointer  cursor-pointer ') + (showHouseSelector ? 'w-[20rem] sm:w-[30rem]' : '[&>*]:pointer-events-none w-[6rem] mb-20') + ' hogwarts-crest-container grid mx-auto duration-500'}>
         <img className='hogwarts-crest-frame' src='images/hogwarts-parts/hogwarts-frame.svg'/>
         <CrestColour house='gryffindor'/>
         <CrestColour house='slytherin'/>
@@ -60,7 +60,7 @@ export default function HouseSelector({ showHouseSelector, toggleHouseSelector }
         <CrestPart house='hufflepuff' />
         <CrestPart house='ravenclaw' />
         <CrestPart house='hogwarts' />
-        <img onAnimationEnd={animateCrest} ref={mist} src='images/hogwarts-parts/imperio-mist.svg' alt='imperio mist' className='w-0 place-self-center z-20'/>
+        <img onAnimationEnd={() => animateCrest()} ref={mist} src='images/hogwarts-parts/imperio-mist.svg' alt='imperio mist' className='w-0 place-self-center z-20'/>
       </div>
     </footer>
   )
--- src/utils/housing.ts
+++ src/utils/housing.ts
@@ -4,7 +4,7 @@ import { House } from "../interfaces/House";
 
 const houses:House[] = ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw'];
 
-export function animateHouse(part: House) {
+export function animateHouse(part: (House | '')) {
   // Add highlight
   houses.forEach((house) => {
     if (part !== house || part === 'hogwarts') document.querySelector(`#${house}-colour`)?.classList.add('opacity-50');
@@ -14,7 +14,7 @@ export function animateHouse(part: House) {
   document.querySelector(`#${part}-part`)?.classList.add('animate');
 }
 
-export function unanimateHouse(part: House) {
+export function unanimateHouse(part: House | '') {
   // Remove hightlight
   houses.forEach((house) => {
     document.querySelector(`#${house}-colour`)?.classList.remove('opacity-50');
