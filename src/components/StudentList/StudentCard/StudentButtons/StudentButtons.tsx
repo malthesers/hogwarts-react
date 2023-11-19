dd49069626f5133defe7209cf8249217cc1971f4
malt9830 <malt9830@stud.kea.dk>
2023-11-19 21:41:25 +0100

    Implemented ts in StudentButtons

--- src/components/StudentList/StudentCard/StudentButtons/StudentButtons.tsx
+++ src/components/StudentList/StudentCard/StudentButtons/StudentButtons.tsx
@@ -1,13 +1,13 @@
+import { Student } from '../../../../interfaces/Student'
 import InquisitorButton from './InquisitorButton'
 import PrefectButton from './PrefectButton'
 import ExpelButton from './ExpelButton'
-import PropTypes from 'prop-types';
 
-StudentButtons.propTypes = {
-  student: PropTypes.object
+interface StudentButtonsProps {
+  student: Student
 }
 
-export function StudentButtons({ student }) {
+export function StudentButtons({ student }: StudentButtonsProps) {
   return (
     <div className={(student.expelled ? 'opacity-30' : '') + ' grid sm:grid-cols-2 md:grid-cols-3 gap-2 my-4 duration-200'}>
       <PrefectButton student={student} />
